12dde37fdc3d3ac072441162abe867bd83d23e15
payne <antilevski@tut.by>
2022-10-08 17:38:50 +0300

    added dynaconf

--- src/password_generator/settings.py
+++ src/password_generator/settings.py
@@ -2,8 +2,6 @@ from pathlib import Path
 
 from dynaconf import settings as _ds
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-
 _this_file = Path(__file__).resolve()
 DIR_PROJECT = _this_file.parent.resolve()
 DIR_SRC = DIR_PROJECT.parent.resolve()
@@ -59,18 +57,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "password_generator.wsgi.application"
 
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
-
-# Password validation
-# https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -87,9 +79,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-# Internationalization
-# https://docs.djangoproject.com/en/3.2/topics/i18n/
-
 LANGUAGE_CODE = "en-us"
 
 TIME_ZONE = "UTC"
@@ -100,12 +89,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.2/howto/static-files/
-
 STATIC_URL = "/static/"
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
