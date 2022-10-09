9f9c4cec85ade2a0192085159814132d042187f7
payne <antilevski@tut.by>
2022-10-09 18:07:40 +0300

    added landing page

--- src/applications/generator/views.py
+++ src/applications/generator/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    return render(request, "generator/home.html", {"password": "sdaksld1232"})
--- src/password_generator/settings.py
+++ src/password_generator/settings.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+
 import dj_database_url
 from dynaconf import settings as _ds
 
