4c401e5fae9a2efd2344b54a299d7f63f64aaf7e
payne <antilevski@tut.by>
2022-10-09 19:49:00 +0300

    added password generator view

--- src/applications/generator/views.py
+++ src/applications/generator/views.py
@@ -1,5 +1,34 @@
+import random
+
 from django.shortcuts import render
 
 
 def home(request):
-    return render(request, "generator/home.html", {"password": "sdaksld1232"})
+    return render(
+        request,
+        "generator/home.html",
+    )
+
+
+def password(request):
+
+    characters = list("abcdefghijklmnopqrstuvwxyz")
+    if request.GET.get("uppercase"):
+        characters.extend(list("ABCDEFGHIJKLMNOPQRSTUVWXYZ"))
+    if request.GET.get("special"):
+        characters.extend(list("!@#$%^&*()_+"))
+    if request.GET.get("numbers"):
+        characters.extend(list("1234567890"))
+
+    length = int(request.GET.get("length"))
+    thepassword = ""
+    for x in range(length):
+        thepassword += random.choice(characters)
+
+    return render(request, "generator/password.html", {"password": thepassword})
+
+
+def about(request):
+    return render(
+        request, "generator/about.html", {"system": request.META.get("DESKTOP_SESSION")}
+    )
