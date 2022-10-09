57aafe6ebdbc6d3e6be714183587ff7757f078e8
swankyalex <72443505+swankyalex@users.noreply.github.com>
2022-10-09 20:13:39 +0300

    v0.1 done (#7)
    * added password generator view
    
    * v0.1

--- src/password_generator/urls.py
+++ src/password_generator/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from applications.generator import views
 
 urlpatterns = [
-    path("", views.home),
+    path("", views.home, name="home"),
+    path("password/", views.password, name="password"),
+    path("about/", views.about, name="about"),
 ]
