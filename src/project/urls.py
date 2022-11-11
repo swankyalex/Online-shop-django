3b258e0157aa8069c63bdf456ef1f57d8678c98a
swankyalex <72443505+swankyalex@users.noreply.github.com>
2022-11-11 16:50:01 +0300

    created fixtures (#10)

--- src/project/urls.py
+++ src/project/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include
 from django.urls import path
@@ -7,3 +9,6 @@ urlpatterns = [
     path("generator/", include("generator.urls")),
     path("", include("products.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
