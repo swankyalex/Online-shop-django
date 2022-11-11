55713b2db0db0338d80355bdbe6210b99efaf40f
payne <antilevski@tut.by>
2022-11-11 15:46:26 +0300

    created models for products

--- src/applications/products/admin.py
+++ src/applications/products/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
 
-from .models import ProductCategory, Product
+from .models import Product
+from .models import ProductCategory
 
 admin.site.register(Product)
 admin.site.register(ProductCategory)
-
-
--- src/applications/products/models.py
+++ src/applications/products/models.py
@@ -14,8 +14,8 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     quantity = models.PositiveIntegerField(default=0)
-    image = models.ImageField(upload_to='products_images')
+    image = models.ImageField(upload_to="products_images")
     category = models.ForeignKey(to=ProductCategory, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f'Продукт: {self.name} | Категория: {self.category.name}'
+        return f"Продукт: {self.name} | Категория: {self.category.name}"
