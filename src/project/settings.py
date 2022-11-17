9d8111e736d27fd9065bc398924e024283661943
payne <antilevski@tut.by>
2022-11-17 20:36:47 +0300

    added github auth

--- src/project/settings.py
+++ src/project/settings.py
@@ -24,7 +24,11 @@ ALLOWED_HOSTS = [
 DOMAIN_NAME = _ds.DOMAIN_NAME
 
 PROJECT_APPS = ["generator", "products", "users"]
-
+THIRD_PARTY_APPS = ['allauth',
+                    'allauth.account',
+                    'allauth.socialaccount',
+                    'allauth.socialaccount.providers.github',
+                    ]
 INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
@@ -32,7 +36,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django.contrib.sites",
     *PROJECT_APPS,
+    *THIRD_PARTY_APPS,
 ]
 
 MIDDLEWARE = [
@@ -115,3 +121,19 @@ EMAIL_PORT = 465
 EMAIL_HOST_USER = "django-study-server@yandex.ru"
 EMAIL_HOST_PASSWORD = _ds.EMAIL_HOST_PASSWORD
 EMAIL_USE_SSL = True
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
+
+SITE_ID = 1
+
+
+SOCIALACCOUNT_PROVIDERS = {
+    'github': {
+        'SCOPE': [
+            'user',
+        ],
+    }
+}
