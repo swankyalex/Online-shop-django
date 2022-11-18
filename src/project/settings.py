f121e0838dbc962fb3fbd7711820155c88c19eb9
payne <antilevski@tut.by>
2022-11-18 16:06:23 +0300

    added tests

--- src/project/settings.py
+++ src/project/settings.py
@@ -75,7 +75,14 @@ WSGI_APPLICATION = "project.wsgi.application"
 
 DATABASE_URL = os.getenv("DATABASE_URL", _ds.DATABASE_URL)
 
-DATABASES = {"default": dj_database_url.parse(DATABASE_URL)}
+#DATABASES = {"default": dj_database_url.parse(DATABASE_URL)}
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': DIR_SRC / 'db.sqlite3',
+    }
+}
 
 AUTH_PASSWORD_VALIDATORS = [
     {
