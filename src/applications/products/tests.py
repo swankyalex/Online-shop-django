00e15f081b6b3be57210853b090d55779dfc539e
payne <antilevski@tut.by>
2022-11-18 15:42:57 +0300

    added tests

--- src/applications/products/tests.py
+++ src/applications/products/tests.py
@@ -1,28 +1,27 @@
 from django.test import TestCase
 from django.urls import reverse
-
 from products.models import Product
 
-class IndexViewTestCase(TestCase):
 
+class IndexViewTestCase(TestCase):
     def test_view(self):
-        path = reverse('products:index')
+        path = reverse("products:index")
         response = self.client.get(path)
 
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.context_data['title'], 'Store')
-        self.assertTemplateUsed(response, 'products/index.html')
+        self.assertEqual(response.context_data["title"], "Store")
+        self.assertTemplateUsed(response, "products/index.html")
 
 
 class ProductsListViewTestCase(TestCase):
-    fixtures = ['categories.json', 'goods.json']
+    fixtures = ["categories.json", "goods.json"]
 
     def test_list(self):
-        path = reverse('products:products')
+        path = reverse("products:products")
         response = self.client.get(path)
 
         products = Product.objects.all()[:3]
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'products/products.html')
-        self.assertEqual(response.context_data['title'], "Store - Каталог")
-        self.assertEqual(list(response.context_data['object_list']), list(products))
+        self.assertTemplateUsed(response, "products/products.html")
+        self.assertEqual(response.context_data["title"], "Store - Каталог")
+        self.assertEqual(list(response.context_data["object_list"]), list(products))
